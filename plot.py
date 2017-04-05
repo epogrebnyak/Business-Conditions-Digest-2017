4fc92a77e8f5fc3ba79ad7cf64a2f9a016a1304c
Evgeny Pogrebnyak <e.pogrebnyak@gmail.com>
2017-04-05 19:21:21 +0300

    Add files via upload

--- plot.py
+++ plot.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 FILE = "BCD.xls"
-df = pd.read_excel(FILE, skip = 1, header = 1, index_col = 'DATE')
+df = pd.read_excel(FILE, skip=1, header=1, index_col='DATE')
 print(df.head())
 
 doc = """GDP5	PROD5
@@ -14,12 +14,35 @@ INV	COMM"""
 pairs = [x.split("\t") for x in doc.split("\n")]
 
 # 6 subplots, the axes array is 1-d
-f, axarr = plt.subplots(6, sharex=True, figsize = (5, 12))
+f, axarr = plt.subplots(6, sharex=True, figsize=(6, 12))
 
 for i in range(6):
     axarr[i].plot(df[pairs[i]])
     title = "+".join(pairs[i])
-    axarr[i].set_title(title)
-    
+    # axarr[i].set_title(title)
 
 plt.savefig('plot.png')
+
+"""
+
+Easy:
+1. Set vertical space between subplots to 0
+2. No ticks on x axis 
+3. Transparent lower and upper boundary of plot for plots 2-5 
+   (is it possible?)
+4. Legend on each subplot, control its location - upper left corner
+5. Vertical grid
+6. Horizontal grid
+7. Global header for the enire plot
+
+Not that easy:
+8. df.GDP5 is by quarter, still want to put it on first subplot
+9. Suggestions to control figure size. Currently figsize=(6, 12)
+   was trail and error.
+
+"""
+
+# not todo
+# - save df to csv or Excel 
+# - bars for recession
+# - delete zeros
